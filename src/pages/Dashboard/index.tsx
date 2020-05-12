13860ee93ec4a72c7de5a2202889c61a0e93642f
Miguel Rios <miguel@adianta.com.br>
2020-05-12 20:51:01 -0300

    feat(error): handling

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -4,7 +4,7 @@ import { FiChevronRight } from 'react-icons/fi';
 import logo from '../../assets/logo.svg';
 import api from '../../services/api';
 
-import { Title, Form, Repositories } from './styles';
+import { Title, Form, Repositories, Error } from './styles';
 
 interface Repository {
 	full_name: string;
@@ -16,29 +16,40 @@ interface Repository {
 }
 
 const Dashboard: React.FC = () => {
-	const [newRepo, setNewRepo] = useState('');
+	const [search, setSearch] = useState('');
+	const [searchError, setSearchError] = useState('');
 	const [repositories, setRepositories] = useState<Repository[]>([]);
 
 	async function addRepository(e: FormEvent<HTMLFormElement>) {
 		e.preventDefault();
-		const response = await api.get<Repository>(`repos/${newRepo}`);
-		setRepositories([...repositories, response.data]);
-		setNewRepo('');
+
+		if (!search) {
+			setSearchError('Busque pelo repositório com o formato <autor>/<repositório>');
+			return;
+		}
+
+		try {
+			const response = await api.get<Repository>(`repos/${search}`);
+			setRepositories([...repositories, response.data]);
+			setSearch('');
+			setSearchError('');
+		} catch (err) {
+			setSearchError('Falha ao buscar repositório');
+		}
 	}
 
 	return (
 		<>
 			<img src={logo} alt="Github Explorer icon" />
+
 			<Title>Explore repositórios no GitHub</Title>
 
-			<Form onSubmit={addRepository}>
-				<input
-					placeholder="Digite aqui"
-					value={newRepo}
-					onChange={e => setNewRepo(e.target.value)}
-				/>
+			<Form onSubmit={addRepository} hasError={!!searchError}>
+				<input placeholder="Digite aqui" value={search} onChange={e => setSearch(e.target.value)} />
 				<button type="submit">Buscar</button>
 			</Form>
+
+			{searchError && <Error>{searchError}</Error>}
 
 			<Repositories>
 				{repositories.map(repo => (

2301fcd4a86aa3e838ca34d573eac474541338c3
Miguel Rios <miguel@adianta.com.br>
2020-05-12 00:14:42 -0300

    feat(data): getting data from github api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,74 +1,58 @@
-import React from 'react';
+import React, { useState, FormEvent } from 'react';
 import { FiChevronRight } from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg';
+import api from '../../services/api';
 
 import { Title, Form, Repositories } from './styles';
 
+interface Repository {
+	full_name: string;
+	description: string;
+	owner: {
+		login: string;
+		avatar_url: string;
+	};
+}
+
 const Dashboard: React.FC = () => {
+	const [newRepo, setNewRepo] = useState('');
+	const [repositories, setRepositories] = useState<Repository[]>([]);
+
+	async function addRepository(e: FormEvent<HTMLFormElement>) {
+		e.preventDefault();
+		const response = await api.get<Repository>(`repos/${newRepo}`);
+		setRepositories([...repositories, response.data]);
+		setNewRepo('');
+	}
+
 	return (
 		<>
 			<img src={logo} alt="Github Explorer icon" />
 			<Title>Explore repositórios no GitHub</Title>
 
-			<Form>
-				<input placeholder="Digite aqui" />
+			<Form onSubmit={addRepository}>
+				<input
+					placeholder="Digite aqui"
+					value={newRepo}
+					onChange={e => setNewRepo(e.target.value)}
+				/>
 				<button type="submit">Buscar</button>
 			</Form>
 
 			<Repositories>
-				<a href="teste">
-					<img
-						src="https://avatars3.githubusercontent.com/u/12296400?s=400&u=12bb04026b457caad8edb63cc24fa28bf92f6c6e&v=4"
-						alt="user avatar"
-					/>
+				{repositories.map(repo => (
+					<a key={repo.full_name} href="teste">
+						<img src={repo.owner.avatar_url} alt={repo.owner.login} />
 
-					<div>
-						<strong>repo-cool-name</strong>
-						<p>descripttion cool também</p>
-					</div>
+						<div>
+							<strong>{repo.full_name}</strong>
+							<p>{repo.description}</p>
+						</div>
 
-					<FiChevronRight size={20} />
-				</a>
-				<a href="teste">
-					<img
-						src="https://avatars3.githubusercontent.com/u/12296400?s=400&u=12bb04026b457caad8edb63cc24fa28bf92f6c6e&v=4"
-						alt="user avatar"
-					/>
-
-					<div>
-						<strong>repo-cool-name</strong>
-						<p>descripttion cool também</p>
-					</div>
-
-					<FiChevronRight size={20} />
-				</a>
-				<a href="teste">
-					<img
-						src="https://avatars3.githubusercontent.com/u/12296400?s=400&u=12bb04026b457caad8edb63cc24fa28bf92f6c6e&v=4"
-						alt="user avatar"
-					/>
-
-					<div>
-						<strong>repo-cool-name</strong>
-						<p>descripttion cool também</p>
-					</div>
-
-					<FiChevronRight size={20} />
-				</a>
-				<a href="teste">
-					<img
-						src="https://avatars3.githubusercontent.com/u/12296400?s=400&u=12bb04026b457caad8edb63cc24fa28bf92f6c6e&v=4"
-						alt="user avatar"
-					/>
-
-					<div>
-						<strong>repo-cool-name</strong>
-						<p>descripttion cool também</p>
-					</div>
-
-					<FiChevronRight size={20} />
-				</a>
+						<FiChevronRight size={20} />
+					</a>
+				))}
 			</Repositories>
 		</>
 	);
